7ff6f5657d2f11685edc168210c67c78f0c49e88
Gabriel Henrique Koch <gabriel_h_koch@estudante.sesisenai.org.br>
2025-03-19 10:12:12 -0300

    COMMIT - atividade 3 terminada

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
 console.log("Hello World")
 
 function verificarPar(numero) {
-    if(numero % 2 === 0) {
+    if (numero % 2 === 0) {
         console.log("par");
     } else {
         console.log("impar");
     }
-}   
+}
 
 verificarPar(4);
 verificarPar(7);
 
-function calcular(n1,n2){
+function calcular(n1, n2) {
 
-    const soma=n1 +n2;
-    const subtracao=n1 - n2;
-    const divisao=n2!== 0 ? n1/n2: "Não pode dividir por zero";
-    const multiplicacao=n1 * n2;
+    const soma = n1 + n2;
+    const subtracao = n1 - n2;
+    const divisao = n2 !== 0 ? n1 / n2 : "Não pode dividir por zero";
+    const multiplicacao = n1 * n2;
 
 
 
-console.log(soma);
-console.log(subtracao);
-console.log(divisao);
-console.log(multiplicacao);
+    console.log(soma);
+    console.log(subtracao);
+    console.log(divisao);
+    console.log(multiplicacao);
 
 }
 
-let n1=parseFloat(prompt("Digite seu primeiro número"));
-let n2=parseFloat(prompt("Digite seu segundo número"));
+let n1 = parseFloat(prompt("Digite seu primeiro número"));
+let n2 = parseFloat(prompt("Digite seu segundo número"));
 
-calcular (n1, n2);
+calcular(n1, n2);
 
 
+let numero = 10;
+let delay = 0;
+while(numero>=1){
 
+    console.log(numero);
+    numero --;
+}
 
 
