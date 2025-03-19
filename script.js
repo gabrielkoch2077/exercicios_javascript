9e4aa48e56c5538221d7c747ec68f59bbed18023
Gabriel Henrique Koch <gabriel_h_koch@estudante.sesisenai.org.br>
2025-03-19 09:57:34 -0300

    COMMIT - atividade 2

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ function verificarPar(numero) {
 
 verificarPar(4);
 verificarPar(7);
+
+function calcular(n1,n2){
+
+    const soma=n1 +n2;
+    const subtracao=n1 - n2;
+    const divisao=n2!== 0 ? n1/n2: "Não pode dividir por zero";
+    const multiplicacao=n1 * n2;
+
+
+
+console.log(soma);
+console.log(subtracao);
+console.log(divisao);
+console.log(multiplicacao);
+
+}
+
+let n1=parseFloat(prompt("Digite seu primeiro número"));
+let n1=parseFloat(prompt("Digite seu segundo número"));
+
+calcular (n1, n2);
+
+
+
+
+
